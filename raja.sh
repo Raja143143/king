8741d2d0f91454dce6445a3ba08f589a7d33d8a6
root <root@ip-192-168-1-72.ec2.internal>
2025-01-06 09:32:44 +0000

    first commit

---
+++ raja.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+apt update
+apt  upgrade -y
+apt install java* -y 
+wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+apt install jenkins -y
+
+systemctl start jenkins
+
+
